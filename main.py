b59277eca381fa400253fd01aea8436304e59d4e
Jake Hennett <jake.hennett@gmail.com>
2024-12-03 16:13:40 -0500

    Iterate over two lists

--- main.py
+++ main.py
@@ -21,7 +21,30 @@ def day01():
     print("day 1 - attempt 1")
     f = open("advent-2024-01-input.txt")
     print("opened file")
-    print(f.readline())
+    # print(f.readline())
+    # for x in f:
+    #    print(x)
+    # full_list = [x for x in f]
+    left_list = []
+    right_list = []
+    for x in f:
+        one_line = x.split()
+        left_list.append(one_line[0])
+        right_list.append(one_line[1])
+    
+    left_list.sort()
+    right_list.sort()
+
+    for x in range(len(left_list)):
+        print(x)
+
+
+    print(left_list)
+    print("done")
+    first_list = []
+
+    #close file
+    f.close()
 
 if __name__ == "__main__":
     main()
