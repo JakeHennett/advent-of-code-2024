81ee3c0f1749811a72b2dcda5ac85157d95ad9be
Jake Hennett <jake.hennett@gmail.com>
2024-12-03 15:38:50 -0500

    boilerplate

---
+++ main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""
+Description of your project.
+"""
+
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Description of your script.")
+    # Add arguments here if needed
+    # parser.add_argument('-f', '--file', help='File to process', required=True)
+    args = parser.parse_args()
+
+    # Your main logic here
+    print("hello world")
+    day01()
+
+def day01():
+    print("day 1")
+
+if __name__ == "__main__":
+    main()
