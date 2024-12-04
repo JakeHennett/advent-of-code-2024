501dc078db7789c181de155bdd4ef371afc63c12
Jake Hennett <jake.hennett@gmail.com>
2024-12-04 11:10:33 -0500

    Find first mul

--- main.py
+++ main.py
@@ -197,8 +197,35 @@ def day02part2():
 
 def day03():
     print("day 3")
-    #f = open("advent-2024-01-input.txt")
+    #f = open("advent-2024-03-input.txt")
     f = open("advent-2024-03-input-small.txt")
+    mul_list=[]
+    for x in f:
+        print("x: " + x)
+        remaining_string=x
+        next_index=remaining_string.find("mul")
+        print("the first occurence of mul begins at: " + str(next_index))
+        new_substring=remaining_string
+        # while next_index > -1:
+        if next_index > -1:
+            new_substring=new_substring[next_index:]
+            print("this should begin with mul: " + new_substring)
+            mul_substring_length=new_substring.find(")")
+            print(mul_substring_length)
+            if mul_substring_length > -1:
+                mul_sub = new_substring[0:mul_substring_length+1]
+                print("individual mul operation: " + mul_sub)
+                mul_list.append(mul_sub)
+            new_substring=new_substring[mul_substring_length:]
+            print(new_substring)
+            next_index=remaining_string.find("mul")
+            print(next_index)
+            new_substring=new_substring[next_index:]
+    
+    mul_sum=0
+    for x in mul_list:
+        print("mul_list item: " + x)
+    print("Sum of all mul operations: " + str(mul_sum))
 
     #close file
     f.close()
