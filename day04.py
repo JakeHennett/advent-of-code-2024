37fc19973421d9ab36bfc8b247883e378c4c0c5f
Jake Hennett <jake.hennett@gmail.com>
2024-12-04 17:12:28 -0500

    Read input as list of strings

---
+++ day04.py
@@ -0,0 +1,23 @@
+#import numpy as np
+
+def day04part1():
+    print("day 4, part 1")
+    # f = open("advent-2024-03-input.txt")
+    f = open("advent-2024-04-input-small.txt")
+    xmas_list=[]
+    for x in f:
+        xmas_list.append(x)
+    xmas_count=0
+    f.close
+    print("XMAS count: " + str(xmas_count))
+
+def day04part2():
+    print("day 4, part 2")
+    # f = open("advent-2024-03-input.txt")
+    f = open("advent-2024-04-input-small.txt")
+    f.close
+    print("TODO")
+
+if __name__ == "__main__":
+    day04part1()
+    day04part2()
