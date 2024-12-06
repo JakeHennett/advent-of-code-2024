a0816f4380f85978bf79256882a659a45a6adef5
Jake Hennett <jake.hennett@gmail.com>
2024-12-06 10:06:57 -0500

    Boilerplate and resources for day 6

---
+++ day06.py
@@ -0,0 +1,10 @@
+import math
+
+def day06part1():
+    print("day 6, part 1")
+    # f = open("advent-2024-06-input.txt")
+    #input_file = open("advent-2024-06-input-small.txt")
+    #rules_file = open("advent-2024-06-rules-small.txt")
+
+if __name__ == "__main__":
+    day06part1()
