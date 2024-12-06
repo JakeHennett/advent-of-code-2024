9f50c6f68ec8b50090a4f1a849ab887d4633f1eb
Jake Hennett <jake.hennett@gmail.com>
2024-12-06 07:48:41 -0500

    Day 5 part 1 correct answer

--- day05.py
+++ day05.py
@@ -10,27 +10,30 @@ def day05part1():
     rules_list=[]
     for x in rules_file:
         rules_list.append(x)
-    print(rules_list)
+    #print(rules_list)
 
     input_list=[]
     for x in input_file:
         input_list.append(x)
-    print(input_list)
+    #print(input_list)
     
     sorted_list=input_list
     middle_average=0
+    correct_middle_sum=0
     for page in sorted_list:
-        print("Checking page containing rules: " + page)
+        #print("Checking page containing rules: " + page)
         page = page.split(',')
         page[len(page)-1]=page[len(page)-1][0:2] #remove newline character from last element
         page_sorted=False
+        loop_count=0
         while (not page_sorted):
+            #print("loop count = " + str(loop_count))
             #TODO: Change this check to a valid check once we know what to look for
             if (page_sorted):
                 print("Page is sorted!")
                 page_sorted=True
             else:
-                print("We must sort the page.")
+                #print("We must sort the page.")
                 page_sorted=True #assuming page is sorted properly unless we find otherwise
                 for rule in rules_list:
                     #break rule into two numbers
@@ -49,27 +52,30 @@ def day05part1():
                     #check if indices are in the correct order
                     if (first_rule_index > second_rule_index):
                         page_sorted=False #at least one rule was not followed
-                        print("Reordering items at " + str(first_rule_index) + " and " + str(second_rule_index) + " because " + page[first_rule_index] + " must come before " + page[second_rule_index])
-                        #l.insert(newindex, l.pop(oldindex))
-                        print(page)
+                        #print("Reordering items at " + str(first_rule_index) + " and " + str(second_rule_index) + " because " + page[first_rule_index] + " must come before " + page[second_rule_index])
+                        #print(page)
                         page.insert(first_rule_index, page.pop(second_rule_index))
-                        print(page)
+                        #print(page)
 
                     #print("Rule items found at indices: " + str(first_rule_index) + " and " + str(second_rule_index))
-            #Check if page already matches rules
-            #iterate over rules list once and edit as necessary
-            #page_sorted=True #TODO: remove this hard coded assertion once logic works
-        print("While loop is finished. Page must be sorted now.")
-        print(page)
+            loop_count+=1
+        #print("While loop is finished. Page must be sorted now.")
+        #print(page)
+        if(loop_count == 1): #correct ordering on the first pass
+            correct_middle_sum+=int(page[math.floor(len(page)/2)])
         middle_element = int(page[math.floor(len(page)/2)])
-        print(middle_element)
+        #print("page length is " + str(len(page)) + " so middle index is " + str(math.floor(len(page)/2)) + " which has a value of...")
+        #print(middle_element)
         middle_average+=middle_element
 
 
     input_file.close
     rules_file.close
     print("Middle average: " + str(middle_average))
+    print("Correct middle sum: " + str(correct_middle_sum))
     #10058 is too high
+    #9362 too high
+    #5087 is just right
 
 def day05part2():
     print("day 5, part 2")
