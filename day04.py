7cad4bf791c3321a68099b08fbb38912382189b0
Jake Hennett <jake.hennett@gmail.com>
2024-12-06 22:00:00 -0500

    Find x's in array

--- day04.py
+++ day04.py
@@ -1,4 +1,5 @@
 import sys
+import numpy as np
 # sys.path.append('C:\\Users\\jakeh\\AppData\\Local\\Programs\\Python\\Python313\\Scripts')
 #sys.path.append('C:\\Users\\jakeh\\AppData\\Local\\Programs\\Python\\*')
 #import NumPy
@@ -6,14 +7,32 @@ import sys
 
 def day04part1():
     print("day 4, part 1")
-    # f = open("advent-2024-03-input.txt")
-    f = open("advent-2024-04-input-small.txt")
-    xmas_list=[]
-    for x in f:
-        xmas_list.append(x)
     xmas_count=0
-    f.close
+    file_name = "advent-2024-04-input-small.txt"
+    #file_name = "advent-2024-04-input.txt"
+    xmas_list_file = open(file_name)
+    string_array=[]
+    #iterate over file to find line count and line length
+    for line in xmas_list_file:
+        string_array.append(line.rstrip())
+    xmas_list_file.close
+
+    xmas_list = np.array(list(map(list, string_array)))
+
+    print(xmas_list)
+
+    for x in range(xmas_list.shape[0]):
+        for y in range(xmas_list.shape[1]):
+            if (xmas_list[x][y]=='X'):
+                print(str(x) + ", " + str(y))
+                local_count=checkSpot(x, y, xmas_list)
+                xmas_count+=local_count
+
     print("XMAS count: " + str(xmas_count))
+
+def checkSpot(x, y, xmas_list):
+    word_count=0
+    return word_count
 
 def day04part2():
     print("day 4, part 2")
