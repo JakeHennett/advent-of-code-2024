1a21ff8bb6eb01f673534ad870f703957018e1b2
Jake Hennett <jake.hennett@gmail.com>
2024-12-08 20:37:47 -0500

    Day 08 boilerplate

---
+++ day08.py
@@ -0,0 +1,11 @@
+import math
+import numpy as np
+
+def day08part1():
+    print("day 08, part 1")
+    #file_name = "advent-2024-08-input-small.txt"
+    file_name = "advent-2024-08-input.txt"
+    map_file = open(file_name)
+    
+if __name__ == "__main__":
+    day08part1()
