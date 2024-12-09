90055085a0aea1ab13635149fa92a952e7c1690f
Jake Hennett <jake.hennett@gmail.com>
2024-12-09 08:45:22 -0500

    Using edge case from reddit

--- day09.py
+++ day09.py
@@ -3,14 +3,15 @@ import numpy as np
 
 def day09part1():
     print("day 09, part 1")
-    file_name = "advent-2024-09-input-small.txt"
+    # file_name = "advent-2024-09-input-small.txt"
     # file_name = "advent-2024-09-input.txt"
+    file_name = "advent-2024-09-input-reddit.txt"
     f = open(file_name)
     checksum=0
     string = f.read()
     input_array=list(string)
-    print(string)
-    print(input_array)
+    # print(string)
+    # print(input_array)
     id_string = ""
     for x in range(len(input_array)):
         item_size=-1
@@ -27,36 +28,42 @@ def day09part1():
             else:
                 file_item+='.'
         id_string+=file_item
-    print(id_string)
+    # print(id_string)
     #00...111...2...333.44.5555.6666.777.888899
 
     # sorted_id_array=list(map(list, id_string))
     sorted_id_array=list(id_string)
-    print(sorted_id_array)
+    # print(sorted_id_array)
     is_sorted=False
-    id_iterator=0
     # reverse_id_iterator=int(id_string.__len__())
     reverse_id_iterator=int(sorted_id_array.__len__())
     while (not is_sorted):
         # is_sorted=True
         # first_blank=id_string.find('.') #change to array
-        first_blank=sorted_id_array.index('.')
-        print("First blank at index: " + str(first_blank))
+        try:
+            first_blank=sorted_id_array.index('.')
+        except:
+            first_blank=sorted_id_array.__len__()
+        # print("First blank at index: " + str(first_blank))
         last_char="."
         while (last_char=="."):
             reverse_id_iterator-=1
-            print("Stepping back through array at index " + str(reverse_id_iterator))
+            # print("Stepping back through array at index " + str(reverse_id_iterator))
             # last_char=id_string.__getitem__(reverse_id_iterator) #change to array
             last_char=sorted_id_array.__getitem__(reverse_id_iterator)
-        print("Last valid character is " + last_char)
-        sorted_id_array[first_blank]=last_char
-        sorted_id_array[reverse_id_iterator]='.'
-        print("After sorting, array looks like this:")
-        print(sorted_id_array)
-        if((first_blank+1) >= (reverse_id_iterator-1)):
+        if(first_blank > reverse_id_iterator):
             is_sorted=True
-    # reversed_id_string=id_string.__reversed__()
-    print(sorted_id_array)
+        else:
+            sorted_id_array[first_blank]=last_char
+            sorted_id_array[reverse_id_iterator]='.'
+        # print("Last valid character is " + last_char)
+        # print("After sorting, array looks like this:")
+        # print(sorted_id_array)
+        # if((first_blank+1) >= (reverse_id_iterator-1)):
+        #     is_sorted=True
+    # print(sorted_id_array)
+    print(''.join(sorted_id_array))
+
 
     #find checksum
     array_iter=0
@@ -68,6 +75,8 @@ def day09part1():
         array_iter+=1
         checksum+=individual_sum
     print(checksum)
+    #89403351457 too low
+    #89403351449 too low
 
 if __name__ == "__main__":
     day09part1()
