86f3b97d70383b6df069bbd2bc6933dac7f4870d
Jake Hennett <jake.hennett@gmail.com>
2024-12-10 11:51:20 -0500

    Day 10 boilerplate

---
+++ day10.py
@@ -0,0 +1,11 @@
+import math
+import numpy as np
+
+def day10part1():
+    print("day 10, part 1")
+    #file_name = "advent-2024-10-input-small.txt"
+    file_name = "advent-2024-10-input.txt"
+    map_file = open(file_name)
+    
+if __name__ == "__main__":
+    day10part1()
